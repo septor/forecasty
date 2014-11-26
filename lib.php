483a24150a7c55200da5bc5c5ecda6517c997123
Patrick Weaver <patrickweaver@gmail.com>
2014-11-26 15:21:17 -0500

    lib file start, more prefs

---
+++ lib.php
@@ -0,0 +1,14 @@
+<?php
+
+function getLocation ( $address )
+{
+    $prepAddr = str_replace(' ','+',$address);
+    $geocode = file_get_contents('http://maps.google.com/maps/api/geocode/json?address='.$prepAddr.'&sensor=false');
+    $output= json_decode($geocode);
+    $latitude = $output->results[0]->geometry->location->lat;
+    $longitude = $output->results[0]->geometry->location->lng;
+
+    return $latitude.','.$longitude;
+}
+
+?>
